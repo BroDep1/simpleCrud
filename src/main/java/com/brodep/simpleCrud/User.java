fe1f170ee64daeb9f640aded50dc38e8b9bafcda
brodep <denmonster08@gmail.com>
2024-04-04 23:07:11 +0200

    Application added to docker-compose.yaml

--- src/main/java/com/brodep/simpleCrud/User.java
+++ src/main/java/com/brodep/simpleCrud/User.java
@@ -5,12 +5,14 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Entity
 @Table(name = "users")
 @Getter
 @Setter
+@NoArgsConstructor
 public class User {
     @Id
     @GeneratedValue
