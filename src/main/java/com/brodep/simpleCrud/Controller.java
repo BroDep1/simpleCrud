99dc88fc33aab9cee989c7283a0d8a5412f11b09
brodep <denmonster08@gmail.com>
2024-04-05 11:43:47 +0200

    Some changes to Controller

--- src/main/java/com/brodep/simpleCrud/Controller.java
+++ src/main/java/com/brodep/simpleCrud/Controller.java
@@ -3,26 +3,46 @@ package com.brodep.simpleCrud;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.Objects;
+
 @RestController
 public class Controller {
+    UserRepo userRepo;
     @Autowired
     public Controller(UserRepo userRepo) {
         this.userRepo = userRepo;
     }
 
-    UserRepo userRepo;
     @GetMapping()
     Iterable<User> getAll(){
         return userRepo.findAll();
     }
+
     @PostMapping()
     void add(@RequestBody User user){
         userRepo.save(user);
     }
 
-    /*
     @GetMapping("/{id}")
-    User getById(PathVariable)
+    User getById(@PathVariable Long id){
+        return userRepo.findById(id).orElse(null);
+    }
 
-     */
+    @PutMapping("/{id}")
+    void edit(@PathVariable Long id, @RequestBody String name) throws NullPointerException{
+        User user = new User();
+        user.setId(id);
+        user.setName(name);
+        userRepo.save(user);
+    }
+
+    @DeleteMapping("/{id}")
+    void deleteById(@PathVariable Long id){
+        userRepo.deleteById(id);
+    }
+
+    @DeleteMapping()
+    void deleteAll(){
+        userRepo.deleteAll();
+    }
 }
